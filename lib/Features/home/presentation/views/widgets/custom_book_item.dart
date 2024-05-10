0fcfb47a7777e02e439c989236fa57b824014d0d
saraUsama9910 <sarausama80@gmail.com>
2024-05-10 14:27:54 +0300

    add chached network image

--- lib/Features/home/presentation/views/widgets/custom_book_item.dart
+++ lib/Features/home/presentation/views/widgets/custom_book_item.dart
@@ -3,8 +3,8 @@ import 'package:flutter/material.dart';
 import '../../../../../core/utils/assets.dart';
 
 class CustomBookImage extends StatelessWidget {
-  const CustomBookImage({Key? key}) : super(key: key);
-
+  const CustomBookImage({Key? key, required this.image}) : super(key: key);
+  final String image;
   @override
   Widget build(BuildContext context) {
     return AspectRatio(
